38cf2415d0f7c9dca89537d90f25bc2d209102ca
Edu Noveli <eduardo.noveli001@outlook.com>
2025-04-24 22:16:51 -0300

    teste de formularios

--- exercicios/ex0025/cadastro.php
+++ exercicios/ex0025/cadastro.php
@@ -1,1 +1,17 @@
-teste
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+
+<body>
+    <?php
+    echo "Teste";
+    ?>
+
+</body>
+
+</html>
